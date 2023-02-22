c5d2e99ba84b283e4369b49c787cb1bd2aa34b32
Hiren Viradiya <hiren@ebytelogic.com>
2023-02-22 17:28:19 +0530

    save logs

--- index.php
+++ index.php
@@ -5,4 +5,4 @@ require_once 'vendor/autoload.php';
 use \Ebl\Logs\LogsDelete;
 
 $data = new LogsDelete();
-return $data->logDelete('/var/www/html/iot-devices/logs/', 'size', 1024);
+// return $data->logSave('/path/logs', 'size', 1024);
--- src/LogsDelete.php
+++ src/LogsDelete.php
@@ -7,11 +7,37 @@ use Exception;
 class LogsDelete
 {
 
-    public function __construct()
+    public function logSave($path, $key, $value)
     {
+        try {
+            $log = '["' . date('r\"') . "] " . $key . " : " . $value . "\n";
+            if (is_dir($path)) {
+                $filesArr = scandir($path);
+                $count = count($filesArr) - 2;
+                if ($handle = opendir($path)) {
+                    while (false !== ($file = readdir($handle))) {
+                        if (preg_match("/^.*\.(log)$/i", $file)) {
+                            $fpath = $path . $file;
+                            $size = filesize($fpath);
+                            if (file_exists($fpath) && $size > 1024 && ($fpath == $path . 'logs.log')) {
+                                rename($fpath, $path . "logs." . $count . '.log');
+                                file_put_contents($path . '/logs.log', '');
+                            }
+                            file_put_contents($path . '/logs.log', $log, FILE_APPEND);
+                        }
+                    }
+                    closedir($handle);
+                }
+            } else {
+                mkdir($path);
+                file_put_contents($path . '/logs.log', $log, FILE_APPEND);
+            }
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
-    public function logDelete($path, $type = "", $value)
+    public function logDelete($path, $type, $value, $day = 1)
     {
         if (is_dir($path)) {
             try {
@@ -22,9 +48,17 @@ class LogsDelete
                             if (file_exists($fpath)) {
                                 // that filemtime returns the time of last modification of the file, instead of creation date.
                                 // if ((time() - $filelastmodified) > 2 * 24 * 3600) {
+                                if ($type == 'both') {
+                                    $filelastmodified = filemtime($fpath);
+                                    $size = filesize($fpath);
+                                    if ($size > $value && (time() - $filelastmodified) > $day * 24 * 3600) {
+                                        unlink($fpath);
+                                    }
+                                }
+
                                 if ($type == 'day') {
                                     $filelastmodified = filemtime($fpath);
-                                    if ((time() - $filelastmodified) > 24 * 3600) {
+                                    if ((time() - $filelastmodified) > $value * 24 * 3600) {
                                         unlink($fpath);
                                     }
                                 }
